6ddaa9d9828b8cec5a1b5c323f7027afbf7d2301
App Academy Student <student@appacademy.io>
2014-06-25 15:32:38 -0400

    add dependent destroy to annotatings from annotation docs

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,5 @@
 class Document < ActiveRecord::Base
+  validates :title, uniqueness: true
   validates :title, :body, :user_id, presence: true
   belongs_to :author, class_name: "User", foreign_key: :user_id
 
@@ -8,11 +9,11 @@ class Document < ActiveRecord::Base
     foreign_key: :source_document_id
   )
 
-  has_many(
-    :references,
-    class_name: "Annotating",
-    foreign_key: :annotation_id
-  )
+  has_many :references,
+            class_name: "Annotating",
+            foreign_key: :annotation_id,
+            dependent: :destroy
+
 
   has_many :annotations, through: :annotatings, source: :annotation
   has_many :referenced_texts, through: :references, source: :source_document

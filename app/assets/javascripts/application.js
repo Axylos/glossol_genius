2e872f83fdd7598434a7faba41c09ade8da7352a
App Academy Student <student@appacademy.io>
2014-06-29 08:53:50 -0400

    fix require tree error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require backbone
 //= require glossol
 //= require_tree ../templates
+//= require_tree ./models
 //= require_tree ./collections
 //= require_tree ./views
 //= require_tree ./routers

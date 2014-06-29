da2120f545a1f533275e5fa8822e7587ef3323dc
App Academy Student <student@appacademy.io>
2014-06-29 09:02:58 -0400

    fix require tree error4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,9 +15,9 @@
 //= require underscore
 //= require backbone
 //= require glossol
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ../templates
-//= require_tree ./views
-//= require_tree ./routers
+//= require_tree models
+//= require_tree collections
+//= require_tree templates
+//= require_tree views
+//= require_tree routers
 //= require_tree ./

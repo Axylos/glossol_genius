6ac69dcf092375bf4d6425073397219394620cca
App Academy Student <student@appacademy.io>
2014-07-06 19:41:55 -0400

    add refs now really works with a few minor bugs

--- app/controllers/api/references_controller.rb
+++ app/controllers/api/references_controller.rb
@@ -7,8 +7,10 @@ class Api::ReferencesController < ApplicationController
   
   def create
     @doc = Document.find(params[:document_id])
-    @doc.referenced_text_ids << params[:reference][:source_document_id]
-    @doc.references.last.source_text = params[:reference][:source_text]
+    @doc.references.new({
+      source_text: params[:reference][:source_text],
+      source_document_id: params[:reference][:source_document_id]
+    })
     if @doc.save
       render json: @doc.references.last
     else
